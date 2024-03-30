38f945a3bc2a6f77a0c529259608bb93707d4209
Ethan Metz <76500833@protonmail.com>
2024-03-30 09:36:52 -0700

    trying to get daisyUI working

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return(
+    
+  
+        <button className="btn">Hello</button>
+  )
+}
+
+export default App;
---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [ require('daisyui')], 
+}
