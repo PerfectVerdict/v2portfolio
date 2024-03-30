9576385f3d2b4367df0cb1945955cbfd5a3d211d
Ethan Metz <76500833@protonmail.com>
2024-03-30 09:59:27 -0700

    styled as h*ck

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css'
-
-function App() {
+import npc1Image from './assets/npc1.jpg'
+export default function App() {
   return(
     
   
-        <button className="btn">Hello</button>
+    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card-body">
+      <p>official portfolio of Ethan Metz.</p>
+      <p>full stack npc.</p>
+    </div>
+
+    <figure><img src={npc1Image} alt="Npc meme with loading spinner on forehead." /></figure>
+  </div>
   )
 }
 
-export default App;
