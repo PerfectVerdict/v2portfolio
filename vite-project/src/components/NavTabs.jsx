16fb3e56267465bb7cb1a4b08d985725b1eefc4f
Ethan Metz <perfectverdict@protonmail.com>
2025-02-27 01:07:18 -0500

    title

--- vite-project/src/components/NavTabs.jsx
+++ vite-project/src/components/NavTabs.jsx
@@ -7,42 +7,43 @@ function NavTabs() {
 
   return (
     <>
-      <ul className="nav nav-tabs">
-        <h3 style={{ marginLeft: "20px", marginRight: "20px", marginTop: "4px" }}>Ethan Metz </h3>
-        <li className="nav-item">
-          <Link
-            to="/Projects"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          // className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
-          >
-            <span className="badge text-bg-info">Projects</span>
-          </Link>
-        </li>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <ul className="nav nav-tabs">
+          <h3 style={{ marginLeft: "20px", marginRight: "20px", marginTop: "4px" }}>Ethan Metz </h3>
+          <li className="nav-item">
+            <Link
+              to="/Projects"
+            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            // className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
+            >
+              <span className="badge text-bg-info">Projects</span>
+            </Link>
+          </li>
 
-        <li className="nav-item">
+          <li className="nav-item">
 
 
-          <Link
-            to="/About"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          // className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
-          >
-            <span className="badge text-bg-secondary">About</span>
+            <Link
+              to="/About"
+            // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            // className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+            >
+              <span className="badge text-bg-secondary">About</span>
 
-          </Link>
-        </li >
+            </Link>
+          </li >
 
-        <li className="nav-item">
-          <Link
-            to="/Contact"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          // className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-          >
-            <span className="badge text-bg-warning">Contact</span>
-          </Link>
-        </li>
-      </ul >
-
+          <li className="nav-item">
+            <Link
+              to="/Contact"
+            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            // className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              <span className="badge text-bg-warning">Contact</span>
+            </Link>
+          </li>
+        </ul >
+      </div>
 
     </>
 
